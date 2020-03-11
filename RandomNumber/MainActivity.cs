53f6e99f567019f96bd7a5543100690537295cbe
JJ <jasonj85@gmail.com>
2020-03-11 19:04:26 +0000

    Random number app

---
+++ RandomNumber/MainActivity.cs
@@ -0,0 +1,39 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using System;
+
+namespace RandomNumber
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        SeekBar seekBar;
+        TextView resultTextView;
+        Button rollButton;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+            
+            seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
+            resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
+            rollButton = FindViewById<Button>(Resource.Id.rollButton);
+            rollButton.Click += RollButton_Click;
+
+            SupportActionBar.Title = "My Random Number";
+        }
+
+        private void RollButton_Click(object sender, System.EventArgs e)
+        {
+            Random random = new Random();
+            int randomNumber = random.Next(seekBar.Progress) + 1;
+            resultTextView.Text = randomNumber.ToString();
+        }
+    }
+}
